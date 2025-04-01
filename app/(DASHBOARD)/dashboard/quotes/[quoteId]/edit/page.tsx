c5faec6b747bcd3aad75bfcaa7791e043dabd575
sloumyyy <giovanni.salcuni12@gmail.com>
2025-04-01 08:23:02 +0200

    Fix bug timeline

--- app/(DASHBOARD)/dashboard/quotes/[quoteId]/edit/page.tsx
+++ app/(DASHBOARD)/dashboard/quotes/[quoteId]/edit/page.tsx
@@ -5,11 +5,22 @@ import { getQuoteById } from '@/actions/quote'
 export default async function QuotesPageEdit({ params }: { params: Promise<{ quoteId: string }> }) {
     const { quoteId } = await params
     const response = await getQuoteById({ id: quoteId })
+
     if (!response?.data?.quote) {
         return (
             <div className="container mx-auto px-4 py-6 max-w-7xl">
                 <div className="flex flex-col items-center justify-center h-96">
                     <h2 className="text-2xl font-semibold mb-2">Devis introuvable</h2>
+                </div>
+            </div>
+        )
+    }
+    if (response.data.quote.status === "CONVERTED") {
+        return (
+            <div className="container mx-auto px-4 py-6 max-w-7xl">
+                <div className="flex flex-col items-center justify-center h-96">
+                    <h2 className="text-2xl font-semibold mb-2">Ce devis a été converti en facture</h2>
+                    <p className="text-muted-foreground">Vous ne pouvez plus modifier ce devis</p>
                 </div>
             </div>
         )
--- app/(DASHBOARD)/dashboard/quotes/data-table-quote.tsx
+++ app/(DASHBOARD)/dashboard/quotes/data-table-quote.tsx
@@ -578,4 +578,3 @@ export default function DataTableQuote({ quotes }: DataTableQuoteProps) {
         </div>
     )
 }
-
