8598e4a0583086cdcd72ac3e52e03cd063dd15c6
sloumyyy <giovanni.salcuni12@gmail.com>
2025-04-02 15:02:07 +0200

    Add metadata generation for user profile page to improve SEO
    - Implemented a new function to generate dynamic metadata for the user profile page, including title and description based on the user's first and last name.
    - This enhancement aims to improve search engine optimization and user experience when viewing the profile.

--- app/(DASHBOARD)/dashboard/[userId]/profile/page.tsx
+++ app/(DASHBOARD)/dashboard/[userId]/profile/page.tsx
@@ -3,6 +3,17 @@ import ProfilePageClient from './profile-page'
 import { getUser } from '@/actions/auth'
 import { UserProfile } from '@/lib/utils'
 
+export async function generateMetadata() {
+    const user = await getUser();
+    if (!user?.id) {
+        return [];
+    }
+    return {
+        title: `Profil ${user.firstName} ${user.lastName} | Factura`,
+        description: `Voir le profil de l'utilisateur ${user.firstName} ${user.lastName}`,
+    }
+}
+
 export default async function ProfilePage() {
     const user = await getUser()
     return (
