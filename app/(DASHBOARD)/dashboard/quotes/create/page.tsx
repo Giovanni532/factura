f499dd4f929e9c7651f6d5607357ba53c2c0524e
sloumyyy <giovanni.salcuni12@gmail.com>
2025-04-02 14:24:49 +0200

    Add metadata for CreateQuotePage to enhance SEO and user experience
    - Introduced metadata for the CreateQuotePage, including title and description for improved search engine optimization.
    - Removed unnecessary console log to clean up the code and enhance readability.

--- app/(DASHBOARD)/dashboard/quotes/create/page.tsx
+++ app/(DASHBOARD)/dashboard/quotes/create/page.tsx
@@ -2,6 +2,12 @@ import { getClientsByUserId } from "@/actions/client"
 import QuoteCreatePage from "./quote-create"
 import { getUser } from "@/actions/auth"
 import { getProductsByUserId } from "@/actions/produit"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Créer un devis | Factura",
+    description: "Créer un devis",
+}
 
 export default async function CreateQuotePage() {
     const user = await getUser()
@@ -10,6 +16,5 @@ export default async function CreateQuotePage() {
     const dataProducts = await getProductsByUserId({ userId: user?.id ?? "" })
     const products = dataProducts?.data?.data.items ?? []
 
-    console.log(clients)
     return <QuoteCreatePage clients={clients} products={products} />
 }
